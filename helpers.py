68e7941cd8b7beada0580ceb9c0a9962df5f3dba
sherhacky <sherwood.hachtman@gmail.com>
2023-12-24 23:12:15 -0800

    Some CRT functions for some reason

--- helpers.py
+++ helpers.py
@@ -51,3 +51,49 @@ def cardinal_to_delta_map():
 
 def fill_tool(grid, coordinate):
     pass
+
+
+def euclidean_algorithm(a, b):
+    r = [a, b]
+    s = [1, 0]
+    t = [0 ,1]
+    while r[-1] != 0:
+        q = r[-2] // r[-1]
+        r.append(r[-2] - q*r[-1])
+        s.append(s[-2] - q*s[-1])
+        t.append(t[-2] - q*t[-1])
+    return r[-2], s[-2], t[-2]
+
+
+# input args of form 
+# (x_1, a_1, n_1), (x_2, a_2, n_2), ...
+# where the simultaneous system is 
+# x_i cong a_i mod n_i
+def crt(seq):
+    a_1, n_1 = seq[0]
+    a_2, n_2 = seq[1]
+    g, m_1, m_2 = euclidean_algorithm(n_1, n_2)
+    result = (
+        a_1 * m_2 * n_2 + a_2 * m_1 * n_1
+    ) % (n_1 * n_2)
+    if len(seq) == 2:
+        return result
+    else:
+        return crt([(result, n_1 * n_2)] + seq[2:])
+
+
+def gcd(seq):
+    g, m_1, m_2 = euclidean_algorithm(abs(seq[0]), abs(seq[1]))
+    if len(seq) == 2:
+        return g
+    else:
+        return gcd([g] + seq[2:])
+
+
+def lcm(seq):
+    n = gcd(seq[:2])
+    p = abs(seq[0] * seq[1]) // n
+    if len(seq) == 2:
+        return p
+    else:
+        return lcm([p] + seq[2:])
